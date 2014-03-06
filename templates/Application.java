3f643a4cc6f83330038eb80066e38cf45b3a7f51
Dave Syer <dsyer@gopivotal.com>
2014-03-06 09:32:42 +0000

    Add @Configuration to Application.java

--- templates/Application.java
+++ templates/Application.java
@@ -3,7 +3,9 @@ package ${packageName};
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @ComponentScan
 @EnableAutoConfiguration
 public class Application {
