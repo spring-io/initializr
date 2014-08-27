b282f66aec5797092119e0b2244ff9ac46023f4c
Stephane Nicoll <snicoll@pivotal.io>
2014-08-27 14:09:01 +0200

    Secure actuator endpoints

--- initializr-service/app.groovy
+++ initializr-service/app.groovy
@@ -3,6 +3,7 @@ package app
 import io.spring.initializr.web.LegacyStsController
 
 @Grab('io.spring.initalizr:initializr:1.0.0.BUILD-SNAPSHOT')
+@Grab('spring-boot-starter-security')
 class InitializerService {
 
 	@Bean
