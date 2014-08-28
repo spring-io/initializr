8fd8099579bb3e15688e7887c952c434a7e08e0c
Stephane Nicoll <snicoll@pivotal.io>
2014-08-28 10:49:48 +0200

    Disable security

--- initializr-service/app.groovy
+++ initializr-service/app.groovy
@@ -3,7 +3,6 @@ package app
 import io.spring.initializr.web.LegacyStsController
 
 @Grab('io.spring.initalizr:initializr:1.0.0.BUILD-SNAPSHOT')
-@Grab('spring-boot-starter-security')
 class InitializerService {
 
 	@Bean
