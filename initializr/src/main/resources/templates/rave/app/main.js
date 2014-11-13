ed6cc2e7303ef29068ee9bd0a2bb9ebc1395bb50
Greg Turnquist <gturnquist@gopivotal.com>
2014-11-12 19:00:50 -0600

    Add ability to add RaveJS to Spring Boot projects.
    * Bare bones, i.e. no pre-loaded modules
    * Supports both Gradle and Maven by creating a build step that will install the modules during the build phase.
    * Underneath src/main/resources/static, it creates:
    ** app/main.js - main entry point to start writing code. Has some handy comments for new devs
    ** package.json - npm-based configuration file including coordinates to install RaveJS
    ** boot.js - only item to pull into templates using a <script> tag. Details found in main.js
    
    This sets the tone for automatically creating demo apps using modern, quality JavaScript tactics while also utilizing RaveJS, Pivotal's zero configuration JS loader.

---
+++ initializr/src/main/resources/templates/rave/app/main.js
@@ -0,0 +1,18 @@
+define(function(require) {
+	'use strict';
+
+	// Start writing your code here, in a nicely contained module.
+
+	// Need to add a JavaScript package?
+	// npm install --save <module> or
+	// RaveJS will auto-load and make it available via require('<module>')
+
+	// Need to launch Rave? Put this in your template/HTML
+	// <script async src="boot.js"/ >
+	// or for Thymeleaf:
+	// <script async="" src="boot.js" />
+
+	// Share and enjoy!
+	// --the Rave team at https://github.com/RaveJS
+
+});
---
+++ initializr/src/main/resources/templates/rave/boot.js
@@ -0,0 +1,1 @@
+(function(d,s){s=(d.head||d.getElementsByTagName('head')[0]).appendChild(d.createElement('script'));s.async=!1;s.src='node_modules/rave/rave.js';}(document));
