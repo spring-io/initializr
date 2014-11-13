1b910353dfb2345bed043999b287f1c820ef0b84
Greg Turnquist <gturnquist@gopivotal.com>
2014-11-13 15:13:31 -0600

    Polish main.js

--- initializr/src/main/resources/templates/rave/app/main.js
+++ initializr/src/main/resources/templates/rave/app/main.js
@@ -1,18 +1,21 @@
-define(function(require) {
-	'use strict';
+// Need to add a JavaScript package?
+// "npm install --save <module>" inside src/main/resources/static
+// RaveJS will auto-load and make it available via require('<module>')
+
+// Need to launch Rave? Put this in your template/HTML
+// <script async src="boot.js" />
+// or for Thymeleaf:
+// <script async="" src="boot.js" />
+
+// Share and enjoy!
+// --the Rave team at https://github.com/RaveJS
+
+'use strict';
+
+module.exports = function() {
 
 	// Start writing your code here, in a nicely contained module.
+	console.log('Greetings from RaveJS!');
 
-	// Need to add a JavaScript package?
-	// npm install --save <module> or
-	// RaveJS will auto-load and make it available via require('<module>')
-
-	// Need to launch Rave? Put this in your template/HTML
-	// <script async src="boot.js"/ >
-	// or for Thymeleaf:
-	// <script async="" src="boot.js" />
-
-	// Share and enjoy!
-	// --the Rave team at https://github.com/RaveJS
-
-});
+	// For more about CommonJS, see http://know.cujojs.com/tutorials/modules/authoring-cjs-modules
+}
