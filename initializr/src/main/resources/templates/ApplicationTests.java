2a9df06559ffe040d2039b22c6124aa14cfd4975
Mark Janssen <mark@praseodym.net>
2015-09-21 14:02:44 +0200

    Convert tabs to 4 spaces in templates

--- initializr/src/main/resources/templates/ApplicationTests.java
+++ initializr/src/main/resources/templates/ApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringApplicationConfiguration(classes = ${applicationName}.class)
 ${testAnnotations}public class ${applicationName}Tests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
--- initializr/src/main/resources/templates/ServletInitializer.java
+++ initializr/src/main/resources/templates/ServletInitializer.java
@@ -5,9 +5,9 @@ import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(${applicationName}.class);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(${applicationName}.class);
+    }
 
 }
