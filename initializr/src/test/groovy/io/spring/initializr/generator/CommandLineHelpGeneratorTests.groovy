3f1b4eca139a1e766d99b2adff8d2d0ef296704a
Stephane Nicoll <snicoll@pivotal.io>
2016-02-01 11:29:22 +0100

    Fix test names

--- initializr/src/test/groovy/io/spring/initializr/generator/CommandLineHelpGeneratorTests.groovy
+++ initializr/src/test/groovy/io/spring/initializr/generator/CommandLineHelpGeneratorTests.groovy
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2015 the original author or authors.
+ * Copyright 2012-2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -28,7 +28,7 @@ import static org.junit.Assert.assertThat
 /**
  * @author Stephane Nicoll
  */
-class CommandLineHelpGeneratorTest {
+class CommandLineHelpGeneratorTests {
 
 	private CommandLineHelpGenerator generator = new CommandLineHelpGenerator()
 
