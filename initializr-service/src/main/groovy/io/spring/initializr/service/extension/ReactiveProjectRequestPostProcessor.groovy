ae2b5967357a82c219f0855459c66ccda190828b
Stephane Nicoll <snicoll@pivotal.io>
2016-07-19 16:33:33 +0200

    Support for experimental reactiver starter
    This commit adds support for the experimental reactive starter. When
    that starter is selected, the version of Spring Framework and Reactor are
    automatically overridden and the java version is forced to 1.8
    
    This starter includes an additional BOM that provides dependency
    management for reactive-related dependencies (typically netty).

---
+++ initializr-service/src/main/groovy/io/spring/initializr/service/extension/ReactiveProjectRequestPostProcessor.groovy
@@ -0,0 +1,32 @@
+package io.spring.initializr.service.extension
+
+import io.spring.initializr.generator.ProjectRequest
+import io.spring.initializr.generator.ProjectRequestPostProcessorAdapter
+import io.spring.initializr.metadata.InitializrMetadata
+
+import org.springframework.stereotype.Component
+
+/**
+ * The "web-reactive" starter is experimental as Spring Boot hasn't integrated Spring 5
+ * yet. This {@link io.spring.initializr.generator.ProjectRequestPostProcessor} forces
+ * Java 8 as well as Spring 5 and Reactor 3.
+ *
+ * @author Stephane Nicoll
+ */
+@Component
+class ReactiveProjectRequestPostProcessor extends ProjectRequestPostProcessorAdapter {
+
+	@Override
+	void postProcessAfterResolution(ProjectRequest request, InitializrMetadata metadata) {
+		if (request.resolvedDependencies.find { it.id.equals('experimental-web-reactive') }) {
+			request.javaVersion = '1.8'
+
+			// No need to override those versions with Gradle
+			if ('maven'.equals(request.build)) {
+				request.buildProperties.versions['spring.version'] = { '5.0.0.BUILD-SNAPSHOT' }
+				request.buildProperties.versions['reactor.version'] = { '3.0.0.BUILD-SNAPSHOT' }
+			}
+		}
+	}
+
+}
