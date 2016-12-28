0c7679125578555ab2b5880631042932ee610e16
Sebastien Deleuze <sdeleuze@pivotal.io>
2016-12-28 11:29:26 +0100

    Upgrade to Kotlin 1.0.6 with kotlin-spring plugin
    The kotlin-spring plugin automatically opens classes annotated or
    meta-annotated with annotations like @Component or @Transactional.

--- initializr-generator/src/main/resources/templates/Application.kt
+++ initializr-generator/src/main/resources/templates/Application.kt
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration<% } %>
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration <% } %>
-open class ${applicationName}
+class ${applicationName}
 
 fun main(args: Array<String>) {
     SpringApplication.run(${applicationName}::class.java, *args)
