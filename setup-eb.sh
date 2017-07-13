2e806a4685bb8e555f316f827dcaaac0fa8c62c4
coestre <christian.oestreich@optum.com>
2017-07-13 12:55:09 -0500

    setup gr8conf profile

--- setup-eb.sh
+++ setup-eb.sh
@@ -2,6 +2,4 @@
 set -x
 set -e
 
-echo "[profile gr8conf]" > ~/.aws/config
-echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> ~/.aws/config
-echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/config
+echo  ~/.aws/config
