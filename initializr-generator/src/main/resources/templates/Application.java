839787d55232e48b6308253266d1c335a6079a66
coestre <christian.oestreich@optum.com>
2017-07-24 22:15:30 -0500

    Adding index controller

--- initializr-generator/src/main/resources/templates/Application.java
+++ initializr-generator/src/main/resources/templates/Application.java
@@ -1,6 +1,9 @@
 package {{packageName}};
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 {{#useSpringBootApplication}}
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 {{/useSpringBootApplication}}
@@ -18,9 +21,15 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 @EnableAutoConfiguration
 {{/useSpringBootApplication}}
+@RestController("/")
 public class {{applicationName}} {
 
 	public static void main(String[] args) {
 		SpringApplication.run({{applicationName}}.class, args);
+	}
+
+	@RequestMapping
+	public ResponseEntity<String> index(){
+		return ResponseEntity.ok("Hello World");
 	}
 }
