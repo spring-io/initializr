18ef480a234f44465ff050ecbbda2d1acc93e23a
Stephane Nicoll <snicoll@pivotal.io>
2018-02-01 10:46:06 +0100

    Use default Kotlin plugin settings
    Rather than override the default, this commit removes the override for
    the incremental compilation support in Maven. We now relies on all
    default now.
    
    Closes gh-584

--- initializr-generator/src/main/java/io/spring/initializr/generator/ProjectRequest.java
+++ initializr-generator/src/main/java/io/spring/initializr/generator/ProjectRequest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2017 the original author or authors.
+ * Copyright 2012-2018 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -238,7 +238,6 @@ public class ProjectRequest extends BasicProjectRequest {
 			if ("kotlin".equals(getLanguage())) {
 				buildProperties.getVersions().put(new VersionProperty("kotlin.version"),
 						() -> kotlinVersion);
-				buildProperties.getMaven().put("kotlin.compiler.incremental", () -> "true");
 			}
 		}
 	}
