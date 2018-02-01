e827a99ac7b1aa335ebf7e6223ca1fe2edc78cb7
Stephane Nicoll <snicoll@pivotal.io>
2018-02-01 16:00:36 +0100

    Restructure Cloud messaging
    This commit restructures the cloud messaging group by removing binder
    specific checkbox for Spring Cloud Bus and Spring Cloud Stream. A new
    "Messaging" group is added with RabbitMQ, Kafka and JMS.
    
    To create a new cloud messaging project, the user should now select the
    technology and a binder. To support Spring Cloud Stream with Kafka
    streams, a dedicated "Kafka Streams" has been introduced.
    
    Closes gh-557

---
+++ initializr-service/src/main/java/io/spring/initializr/service/extension/SpringCloudMessagingRequestPostProcessor.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.spring.initializr.service.extension;
+
+import io.spring.initializr.generator.ProjectRequest;
+import io.spring.initializr.metadata.Dependency;
+import io.spring.initializr.metadata.InitializrMetadata;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Determine the appropriate Spring Cloud stream dependency to use based on the
+ * selected messaging technology.
+ * <p>
+ * Does not replace the messaging technology jar by the relevant binder. If more than
+ * one tech is selected, it is far more easier to remove the unnecessary binder jar than
+ * to figure out the name of the tech jar to add to keep support for that technology.
+ *
+ * @author Stephane Nicoll
+ */
+@Component
+public class SpringCloudMessagingRequestPostProcessor
+		extends AbstractProjectRequestPostProcessor {
+
+	static final Dependency KAFKA_BINDER = Dependency.withId("cloud-stream-binder-kafka",
+			"org.springframework.cloud", "spring-cloud-stream-binder-kafka");
+
+	static final Dependency RABBIT_BINDER = Dependency.withId(
+			"cloud-stream-binder-rabbit", "org.springframework.cloud",
+			"spring-cloud-stream-binder-rabbit");
+
+	@Override
+	public void postProcessAfterResolution(ProjectRequest request,
+			InitializrMetadata metadata) {
+		boolean hasSpringCloudStream = hasDependency(request, "cloud-stream");
+		boolean hasReactiveSpringCloudStream = hasDependency(request,
+				"reactive-cloud-stream");
+		boolean hasSpringCloudBus = hasDependency(request, "cloud-bus");
+		boolean hasSpringCloudTurbineStream = hasDependency(request,
+				"cloud-turbine-stream");
+		if (hasSpringCloudStream || hasReactiveSpringCloudStream || hasSpringCloudBus
+				|| hasSpringCloudTurbineStream) {
+			if (hasDependencies(request, "amqp")) {
+				request.getResolvedDependencies().add(RABBIT_BINDER);
+			}
+			if (hasDependencies(request, "kafka")) {
+				request.getResolvedDependencies().add(KAFKA_BINDER);
+			}
+		}
+	}
+
+}
+
---
+++ initializr-service/src/test/java/io/spring/initializr/service/extension/SpringCloudMessagingRequestPostProcessorTests.java
@@ -0,0 +1,181 @@
+/*
+ * Copyright 2012-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.spring.initializr.service.extension;
+
+import io.spring.initializr.generator.ProjectRequest;
+import org.junit.Test;
+
+import static io.spring.initializr.service.extension.SpringCloudMessagingRequestPostProcessor.KAFKA_BINDER;
+import static io.spring.initializr.service.extension.SpringCloudMessagingRequestPostProcessor.RABBIT_BINDER;
+
+/**
+ * Tests for {@link SpringCloudMessagingRequestPostProcessor}.
+ *
+ * @author Stephane Nicoll
+ */
+public class SpringCloudMessagingRequestPostProcessorTests
+		extends AbstractRequestPostProcessorTests {
+
+	@Test
+	public void springCloudStreamWithRabbit() {
+		ProjectRequest request = createProjectRequest("cloud-stream", "amqp");
+		generateMavenPom(request)
+				.hasDependency(getDependency("cloud-stream"))
+				.hasDependency(getDependency("amqp"))
+				.hasDependency(RABBIT_BINDER)
+				.hasSpringBootStarterTest()
+				.hasDependenciesCount(4);
+	}
+
+	@Test
+	public void springCloudStreamWithKafka() {
+		ProjectRequest request = createProjectRequest("cloud-stream", "kafka");
+		generateMavenPom(request)
+				.hasDependency(getDependency("cloud-stream"))
+				.hasDependency(getDependency("kafka"))
+				.hasDependency(KAFKA_BINDER)
+				.hasSpringBootStarterTest()
+				.hasDependenciesCount(4);
+	}
+
+	@Test
+	public void springCloudStreamWithAllBinders() {
+		ProjectRequest request = createProjectRequest("cloud-stream", "amqp", "kafka");
+		generateMavenPom(request)
+				.hasDependency(getDependency("cloud-stream"))
+				.hasDependency(getDependency("amqp"))
+				.hasDependency(getDependency("kafka"))
+				.hasDependency(RABBIT_BINDER)
+				.hasDependency(KAFKA_BINDER)
+				.hasSpringBootStarterTest()
+				.hasDependenciesCount(6);
+	}
+
+	@Test
+	public void reactiveSpringCloudStreamWithRabbit() {
+		ProjectRequest request = createProjectRequest("reactive-cloud-stream", "amqp");
+		request.setBootVersion("2.0.0.BUILD-SNAPSHOT");
+		generateMavenPom(request)
+				.hasDependency(getDependency("reactive-cloud-stream"))
+				.hasDependency(getDependency("amqp"))
+				.hasDependency(RABBIT_BINDER)
+				.hasSpringBootStarterTest()
+				.hasDependenciesCount(4);
+	}
+
+	@Test
+	public void reactiveSpringCloudStreamWithKafka() {
+		ProjectRequest request = createProjectRequest("reactive-cloud-stream", "kafka");
+		request.setBootVersion("2.0.0.BUILD-SNAPSHOT");
+		generateMavenPom(request)
+				.hasDependency(getDependency("reactive-cloud-stream"))
+				.hasDependency(getDependency("kafka"))
+				.hasDependency(KAFKA_BINDER)
+				.hasSpringBootStarterTest()
+				.hasDependenciesCount(4);
+	}
+
+	@Test
+	public void reactiveSpringCloudStreamWithAllBinders() {
+		ProjectRequest request = createProjectRequest("reactive-cloud-stream", "amqp",
+				"kafka");
+		request.setBootVersion("2.0.0.BUILD-SNAPSHOT");
+		generateMavenPom(request)
+				.hasDependency(getDependency("reactive-cloud-stream"))
+				.hasDependency(getDependency("amqp"))
+				.hasDependency(getDependency("kafka"))
+				.hasDependency(RABBIT_BINDER)
+				.hasDependency(KAFKA_BINDER)
+				.hasSpringBootStarterTest()
+				.hasDependenciesCount(6);
+	}
+
+	@Test
+	public void springCloudBusWithRabbit() {
+		ProjectRequest request = createProjectRequest("cloud-bus", "amqp");
+		generateMavenPom(request)
+				.hasDependency(getDependency("cloud-bus"))
+				.hasDependency(getDependency("amqp"))
+				.hasDependency(RABBIT_BINDER)
+				.hasSpringBootStarterTest()
+				.hasDependenciesCount(4);
+	}
+
+	@Test
+	public void springCloudBusWithKafka() {
+		ProjectRequest request = createProjectRequest("cloud-bus", "amqp");
+		generateMavenPom(request)
+				.hasDependency(getDependency("cloud-bus"))
+				.hasDependency(getDependency("amqp"))
+				.hasDependency(RABBIT_BINDER)
+				.hasSpringBootStarterTest()
+				.hasDependenciesCount(4);
+	}
+
+	@Test
+	public void springCloudBusWithAllBinders() {
+		ProjectRequest request = createProjectRequest("cloud-bus", "amqp", "kafka");
+		generateMavenPom(request)
+				.hasDependency(getDependency("cloud-bus"))
+				.hasDependency(getDependency("amqp"))
+				.hasDependency(getDependency("kafka"))
+				.hasDependency(RABBIT_BINDER)
+				.hasDependency(KAFKA_BINDER)
+				.hasSpringBootStarterTest()
+				.hasDependenciesCount(6);
+	}
+
+	@Test
+	public void springCloudTurbineStreamWithRabbit() {
+		ProjectRequest request = createProjectRequest("cloud-turbine-stream", "amqp");
+		request.setBootVersion("2.0.0.BUILD-SNAPSHOT");
+		generateMavenPom(request)
+				.hasDependency(getDependency("cloud-turbine-stream"))
+				.hasDependency(getDependency("amqp"))
+				.hasDependency(RABBIT_BINDER)
+				.hasSpringBootStarterTest()
+				.hasDependenciesCount(4);
+	}
+
+	@Test
+	public void springCloudTurbineStreamWithKafka() {
+		ProjectRequest request = createProjectRequest("cloud-turbine-stream", "kafka");
+		request.setBootVersion("2.0.0.BUILD-SNAPSHOT");
+		generateMavenPom(request)
+				.hasDependency(getDependency("cloud-turbine-stream"))
+				.hasDependency(getDependency("kafka"))
+				.hasDependency(KAFKA_BINDER)
+				.hasSpringBootStarterTest()
+				.hasDependenciesCount(4);
+	}
+
+	@Test
+	public void springCloudTurbineStreamWithAllBinders() {
+		ProjectRequest request = createProjectRequest("cloud-turbine-stream", "amqp",
+				"kafka");
+		request.setBootVersion("2.0.0.BUILD-SNAPSHOT");
+		generateMavenPom(request)
+				.hasDependency(getDependency("cloud-turbine-stream"))
+				.hasDependency(getDependency("amqp"))
+				.hasDependency(getDependency("kafka"))
+				.hasDependency(RABBIT_BINDER)
+				.hasDependency(KAFKA_BINDER)
+				.hasSpringBootStarterTest()
+				.hasDependenciesCount(6);
+	}
+
+}
