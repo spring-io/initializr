26e5b1a31fa9c8b4d53fc17a8540427b9f86b626
weiping <wepa@microsoft.com>
2018-04-26 15:58:54 +0800

    change to use the styles passed from front

--- initializr-service/src/main/java/io/spring/initializr/service/extension/SpringCloudMicroservicePostprocessor.java
+++ initializr-service/src/main/java/io/spring/initializr/service/extension/SpringCloudMicroservicePostprocessor.java
@@ -14,17 +14,18 @@ public class SpringCloudMicroservicePostprocessor extends AbstractProjectRequest
     private static final Map<String, List<String>> serviceToDependencies = new HashMap<>();
 
     static {
-        serviceToDependencies.put("config", Arrays.asList("cloud-config-server", "cloud-eureka"));
-        serviceToDependencies.put("gateway", Arrays.asList("cloud-gateway"));
-        serviceToDependencies.put("discovery", Arrays.asList("cloud-eureka-server"));
+        serviceToDependencies.put("cloud-config-server", Arrays.asList("cloud-config-server"));
+        serviceToDependencies.put("cloud-gateway", Arrays.asList("cloud-gateway"));
+        serviceToDependencies.put("cloud-eureka-server", Arrays.asList("cloud-eureka-server"));
+        serviceToDependencies.put("azure-service-bus", Arrays.asList("azure-service-bus"));
     }
 
     @Override
     public void postProcessAfterResolution(ProjectRequest request,
                                            InitializrMetadata metadata) {
-        for(String service: request.getServices()){
+        for(String service: request.getStyle()){
             if(!serviceToDependencies.containsKey(service)){
-                throw new RuntimeException("Unsupported service type");
+                throw new IllegalStateException("Unsupported service type " + service);
             }
 
             ProjectRequest subModule = new ProjectRequest(request);
