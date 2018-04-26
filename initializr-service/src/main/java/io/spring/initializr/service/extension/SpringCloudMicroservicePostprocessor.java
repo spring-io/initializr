d6fa1a4d339e38776f3f73ca443b9b6b65401ea6
Yawei Wang (DEVDIV) <yaweiw@microsoft.com>
2018-04-26 16:59:59 +0800

    minor fix

--- initializr-service/src/main/java/io/spring/initializr/service/extension/SpringCloudMicroservicePostprocessor.java
+++ initializr-service/src/main/java/io/spring/initializr/service/extension/SpringCloudMicroservicePostprocessor.java
@@ -2,12 +2,13 @@ package io.spring.initializr.service.extension;
 
 import io.spring.initializr.generator.ProjectRequest;
 import io.spring.initializr.generator.ProjectRequestPostProcessor;
-import io.spring.initializr.metadata.Dependency;
 import io.spring.initializr.metadata.InitializrMetadata;
 import org.springframework.stereotype.Component;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Component
 public class SpringCloudMicroservicePostprocessor extends AbstractProjectRequestPostProcessor implements ProjectRequestPostProcessor {
@@ -17,6 +18,7 @@ public class SpringCloudMicroservicePostprocessor extends AbstractProjectRequest
         serviceToDependencies.put("cloud-config-server", Arrays.asList("cloud-config-server"));
         serviceToDependencies.put("cloud-gateway", Arrays.asList("cloud-gateway"));
         serviceToDependencies.put("cloud-eureka-server", Arrays.asList("cloud-eureka-server"));
+        serviceToDependencies.put("cloud-hystrix-dashboard", Arrays.asList("cloud-hystrix-dashboard"));
         serviceToDependencies.put("azure-service-bus", Arrays.asList("azure-service-bus"));
     }
 
