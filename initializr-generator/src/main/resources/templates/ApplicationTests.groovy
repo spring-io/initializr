31433c4511e8769ce9a184a00d401fb3f3e8c990
Unknown <jurrian.fahner@gmail.com>
2018-10-14 22:49:31 +0200

    Replace junit4 for junit5 when version >= 2.1.0.M4
    Sam Brannen showed in his presentation (https://youtu.be/K7g2HUhWbNE) that for springboot 2.1.0 @ExtendWith is not needed anymore. I discovered that spring initializer creates a junit 4 test as a default to demonstrate that
    the context loads.
    
    Since the advent of junit 5 jupiter, I think we should opt to have make default test to use junit 5.
    
    That inspired me to change the spring initializer. I've changed the following:
    - maven pom, to include junit jupiter
    - gradle build, to include junit jupiter
    - java test, update test imports and remove @RunWith
    - kotlin test, update test imports and remove @RunWith
    - groovy test, update test imports and remove @RunWith
    
    I didn't saw an test to test the context loads test, so I preformed it manually. 
    
    Please let me know if I need to change something or need to add a test to test the generated testclass.
    
    I thought that this change is trivial one and a ticket is not needed, however if I'm mistaken, please let me know

--- initializr-generator/src/main/resources/templates/ApplicationTests.groovy
+++ initializr-generator/src/main/resources/templates/ApplicationTests.groovy
@@ -1,10 +1,14 @@
 package {{packageName}}
 
+{{^jupiterAvailable}}
 import org.junit.Test
 import org.junit.runner.RunWith
+{{/jupiterAvailable}}
 {{testImports}}
 {{#newTestInfrastructure}}
+{{^jupiterAvailable}}
 @RunWith(SpringRunner)
+{{/jupiterAvailable}}
 @SpringBootTest
 {{/newTestInfrastructure}}
 {{^newTestInfrastructure}}
--- initializr-generator/src/main/resources/templates/ApplicationTests.java
+++ initializr-generator/src/main/resources/templates/ApplicationTests.java
@@ -1,10 +1,14 @@
 package {{packageName}};
 
+{{^jupiterAvailable}}
 import org.junit.Test;
 import org.junit.runner.RunWith;
+{{/jupiterAvailable}}
 {{testImports}}
 {{#newTestInfrastructure}}
+{{^jupiterAvailable}}
 @RunWith(SpringRunner.class)
+{{/jupiterAvailable}}
 @SpringBootTest
 {{/newTestInfrastructure}}
 {{^newTestInfrastructure}}
