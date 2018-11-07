c05ed7cf38f5545ec3aa1a106d2721d49cb506d0
Stephane Nicoll <snicoll@pivotal.io>
2018-11-07 08:17:03 +0100

    Complete migration to Spring Boot 2.1.0

--- initializr-service/src/main/java/io/spring/initializr/service/InitializrService.java
+++ initializr-service/src/main/java/io/spring/initializr/service/InitializrService.java
@@ -16,8 +16,6 @@
 
 package io.spring.initializr.service;
 
-import java.util.concurrent.Executor;
-
 import io.spring.initializr.metadata.InitializrMetadataProvider;
 import io.spring.initializr.web.project.LegacyStsController;
 
@@ -25,10 +23,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.AsyncConfigurerSupport;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.resource.ResourceUrlProvider;
 
 /**
@@ -38,6 +33,7 @@ import org.springframework.web.servlet.resource.ResourceUrlProvider;
  */
 @SpringBootApplication
 @EnableCaching
+@EnableAsync
 public class InitializrService {
 
 	public static void main(String[] args) {
@@ -49,25 +45,6 @@ public class InitializrService {
 			InitializrMetadataProvider metadataProvider,
 			ResourceUrlProvider resourceUrlProvider) {
 		return new LegacyStsController(metadataProvider, resourceUrlProvider);
-	}
-
-	/**
-	 * {@link Executor} configuration for event processing.
-	 */
-	@Configuration
-	@EnableAsync
-	static class AsyncConfiguration extends AsyncConfigurerSupport {
-
-		@Override
-		public Executor getAsyncExecutor() {
-			ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-			executor.setCorePoolSize(1);
-			executor.setMaxPoolSize(5);
-			executor.setThreadNamePrefix("initializr-");
-			executor.initialize();
-			return executor;
-		}
-
 	}
 
 }
