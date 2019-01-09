51dbc922978b34726d775dd76e41cc05a5793399
Stephane Nicoll <snicoll@pivotal.io>
2019-01-09 15:18:08 +0100

    Polish "Remove surrounding brackets for gradle dependencies"
    Closes gh-797

--- initializr-web/src/test/java/io/spring/initializr/web/project/ProjectGenerationSmokeTests.java
+++ initializr-web/src/test/java/io/spring/initializr/web/project/ProjectGenerationSmokeTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2018 the original author or authors.
+ * Copyright 2012-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -95,10 +95,9 @@ public class ProjectGenerationSmokeTests
 		page.type("gradle-project");
 		page.submit();
 		assertSimpleProject().isGradleProject().gradleBuildAssert()
+				.contains("implementation 'org.springframework.boot:spring-boot-starter'")
 				.contains(
-						"implementation('org.springframework.boot:spring-boot-starter')")
-				.contains(
-						"testImplementation('org.springframework.boot:spring-boot-starter-test')");
+						"testImplementation 'org.springframework.boot:spring-boot-starter-test'");
 	}
 
 	@Test
