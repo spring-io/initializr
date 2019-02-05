857923b549b577ba7739f59331bd38f23513086c
Madhura Bhave <mbhave@pivotal.io>
2019-02-05 13:17:22 -0800

    Remove ProjectGenerationMetricsListener
    Fixes gh-809

--- initializr-actuator/src/test/java/io/spring/initializr/actuate/ActuatorIntegrationTests.java
+++ initializr-actuator/src/test/java/io/spring/initializr/actuate/ActuatorIntegrationTests.java
@@ -16,10 +16,6 @@
 
 package io.spring.initializr.actuate;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.fasterxml.jackson.databind.JsonNode;
 import io.spring.initializr.web.AbstractFullStackInitializrIntegrationTests;
 import io.spring.initializr.web.AbstractInitializrIntegrationTests.Config;
 import org.junit.jupiter.api.Test;
@@ -36,7 +32,7 @@ import static org.assertj.core.api.Assertions.assertThat;
  * @author Stephane Nicoll
  */
 @ActiveProfiles("test-default")
-@SpringBootTest(classes = Config.class, webEnvironment = WebEnvironment.RANDOM_PORT, properties = "management.endpoints.web.exposure.include=info,metrics")
+@SpringBootTest(classes = Config.class, webEnvironment = WebEnvironment.RANDOM_PORT, properties = "management.endpoints.web.exposure.include=info")
 class ActuatorIntegrationTests extends AbstractFullStackInitializrIntegrationTests {
 
 	@Test
@@ -45,59 +41,6 @@ class ActuatorIntegrationTests extends AbstractFullStackInitializrIntegrationTes
 				String.class);
 		assertThat(body).contains("\"spring-boot\"");
 		assertThat(body).contains("\"version\":\"2.1.4.RELEASE\"");
-	}
-
-	@Test
-	void metricsAreRegistered() {
-		downloadZip("/starter.zip?packaging=jar&javaVersion=1.8&style=web&style=jpa");
-		JsonNode result = metricsEndpoint();
-		JsonNode names = result.get("names");
-		List<String> metrics = new ArrayList<>();
-		for (JsonNode name : names) {
-			metrics.add(name.textValue());
-		}
-		assertThat(metrics).contains("initializr.requests", "initializr.packaging.jar",
-				"initializr.java_version.1_8", "initializr.dependency.web",
-				"initializr.dependency.data-jpa");
-
-		int requests = metricValue("initializr.requests");
-		int packaging = metricValue("initializr.packaging.jar");
-		int javaVersion = metricValue("initializr.java_version.1_8");
-		int webDependency = metricValue("initializr.dependency.web");
-		int jpaDependency = metricValue("initializr.dependency.data-jpa");
-
-		// No jpa dep this time
-		downloadZip("/starter.zip?packaging=jar&javaVersion=1.8&style=web");
-
-		assertThat(metricValue("initializr.requests"))
-				.as("Number of request should have increased").isEqualTo(requests + 1);
-		assertThat(metricValue("initializr.packaging.jar"))
-				.as("jar packaging metric should have increased")
-				.isEqualTo(packaging + 1);
-		assertThat(metricValue("initializr.java_version.1_8"))
-				.as("java version metric should have increased")
-				.isEqualTo(javaVersion + 1);
-		assertThat(metricValue("initializr.dependency.web"))
-				.as("web dependency metric should have increased")
-				.isEqualTo(webDependency + 1);
-		assertThat(metricValue("initializr.dependency.data-jpa"))
-				.as("jpa dependency metric should not have increased")
-				.isEqualTo(jpaDependency);
-	}
-
-	private JsonNode metricsEndpoint() {
-		return parseJson(getRestTemplate().getForObject(createUrl("/actuator/metrics"),
-				String.class));
-	}
-
-	private int metricValue(String metric) {
-		JsonNode root = parseJson(getRestTemplate()
-				.getForObject(createUrl("/actuator/metrics/" + metric), String.class));
-		JsonNode measurements = root.get("measurements");
-		assertThat(measurements.isArray());
-		assertThat(measurements.size()).isEqualTo(1);
-		JsonNode measurement = measurements.get(0);
-		return measurement.get("value").intValue();
 	}
 
 }
