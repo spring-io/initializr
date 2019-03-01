0b61f5145abae6fa9b25d512beb055810c52ce42
Madhura Bhave <mbhave@pivotal.io>
2019-03-01 14:22:04 -0800

    Add logging to track caching on CI

--- ci/scripts/build-project.sh
+++ ci/scripts/build-project.sh
@@ -3,7 +3,11 @@ set -e
 
 source $(dirname $0)/common.sh
 repository=$(pwd)/distribution-repository
-
+if [ -z "$(ls -A maven)" ]; then
+   echo "Maven cache not available."
+else
+   echo "Maven cache found."
+fi
 pushd git-repo > /dev/null
 ./mvnw clean deploy -U -Pfull -DaltDeploymentRepository=distribution::default::file://${repository}
 popd > /dev/null
