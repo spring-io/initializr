1d213258a25733a97dbbe377498cea1a49ee7fca
Stephane Nicoll <snicoll@pivotal.io>
2019-09-12 16:50:05 +0200

    Upgrade to Gradle 5.6.2

--- initializr-generator-spring/src/test/java/io/spring/initializr/generator/spring/build/gradle/GradleKtsProjectGenerationConfigurationTests.java
+++ initializr-generator-spring/src/test/java/io/spring/initializr/generator/spring/build/gradle/GradleKtsProjectGenerationConfigurationTests.java
@@ -84,7 +84,7 @@ class GradleKtsProjectGenerationConfigurationTests {
 	}
 
 	static Stream<Arguments> gradleWrapperParameters() {
-		return Stream.of(Arguments.arguments("2.1.3.RELEASE", "5.6"));
+		return Stream.of(Arguments.arguments("2.1.3.RELEASE", "5.6.2"));
 	}
 
 	@ParameterizedTest(name = "Spring Boot {0}")
