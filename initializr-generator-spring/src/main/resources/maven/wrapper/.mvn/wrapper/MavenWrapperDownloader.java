5e074e5eeedfb93a3abe91ead7127c336769b878
Nils Breunese <nbreunese@bol.com>
2020-01-02 10:43:47 +0100

    Upgrade to Maven Wrapper 0.5.6 and Maven 3.6.3
    See gh-1047

--- initializr-generator-spring/src/main/resources/maven/wrapper/.mvn/wrapper/MavenWrapperDownloader.java
+++ initializr-generator-spring/src/main/resources/maven/wrapper/.mvn/wrapper/MavenWrapperDownloader.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2007-present the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -20,7 +20,7 @@ import java.util.Properties;
 
 public class MavenWrapperDownloader {
 
-    private static final String WRAPPER_VERSION = "0.5.5";
+    private static final String WRAPPER_VERSION = "0.5.6";
     /**
      * Default URL to download the maven-wrapper.jar from, if no 'downloadUrl' is provided.
      */
