9255ae9fbeff422e71e83992f1a2e83c3120a462
Stephane Nicoll <snicoll@pivotal.io>
2020-05-17 11:25:23 +0200

    Polish

--- initializr-generator-test/src/test/java/io/spring/initializr/generator/test/project/JvmModuleAssertTests.java
+++ initializr-generator-test/src/test/java/io/spring/initializr/generator/test/project/JvmModuleAssertTests.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2020 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -136,7 +136,7 @@ class JvmModuleAssertTests {
 				.isThrownBy(() -> assertThat(forJavaProject(root)).testSource("com.acme", "DemoTests"));
 	}
 
-	private AssertProvider<AbstractJvmModuleAssert> forJavaProject(Path root) {
+	private AssertProvider<AbstractJvmModuleAssert<?>> forJavaProject(Path root) {
 		return () -> new JvmModuleAssert(root, JAVA_LANGUAGE);
 	}
 
