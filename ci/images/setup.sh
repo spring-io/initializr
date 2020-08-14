1113fc0dd8562c187ce7c90a3c688ebace58ede3
Stephane Nicoll <snicoll@pivotal.io>
2020-08-14 08:06:38 +0200

    Upgrade Java 8 version in CI image

--- ci/images/setup.sh
+++ ci/images/setup.sh
@@ -15,7 +15,7 @@ curl https://raw.githubusercontent.com/spring-io/concourse-java-scripts/v0.0.3/c
 ###########################################################
 # JAVA
 ###########################################################
-JDK_URL=https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u202-b08/OpenJDK8U-jdk_x64_linux_hotspot_8u202b08.tar.gz
+JDK_URL=https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u265-b01/OpenJDK8U-jdk_x64_linux_hotspot_8u265b01.tar.gz
 
 mkdir -p /opt/openjdk
 cd /opt/openjdk
