c9fd0bffdf91b74f6750065621e49748af601ac2
Stephane Nicoll <snicoll@vmware.com>
2020-12-06 15:54:17 +0100

    Polish

--- initializr-generator-spring/src/test/java/io/spring/initializr/generator/spring/code/SourceCodeProjectGenerationConfigurationTests.java
+++ initializr-generator-spring/src/test/java/io/spring/initializr/generator/spring/code/SourceCodeProjectGenerationConfigurationTests.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.spring.initializr.generator.spring.code;
 
 import io.spring.initializr.generator.language.CompilationUnit;
