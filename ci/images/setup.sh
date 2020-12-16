2fe35082648b1e07bfb07dc6356880cf67664c53
Stephane Nicoll <snicoll@vmware.com>
2020-12-16 10:04:13 +0100

    Upgrade Java 8 version in CI image

--- ci/images/setup.sh
+++ ci/images/setup.sh
@@ -18,7 +18,7 @@ curl https://raw.githubusercontent.com/spring-io/concourse-java-scripts/v0.0.3/c
 ###########################################################
 # JAVA
 ###########################################################
-JDK_URL=https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u265-b01/OpenJDK8U-jdk_x64_linux_hotspot_8u265b01.tar.gz
+JDK_URL=https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u275-b01/OpenJDK8U-jdk_x64_linux_hotspot_8u275b01.tar.gz
 
 mkdir -p /opt/openjdk
 cd /opt/openjdk
