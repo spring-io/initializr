35d40e3619986b789ecde803c01eb0fae15fd1f0
Stephane Nicoll <snicoll@vmware.com>
2020-12-17 07:11:59 +0100

    Harmonize CI pipeline

--- ci/scripts/promote.sh
+++ ci/scripts/promote.sh
@@ -1,27 +1,16 @@
 #!/bin/bash
-set -e
 
 source $(dirname $0)/common.sh
+CONFIG_DIR=git-repo/ci/config
 
-buildName=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.name' )
-buildNumber=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.number' )
-groupId=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.modules[0].id' | sed 's/\(.*\):.*:.*/\1/' )
 version=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.modules[0].id' | sed 's/.*:.*:\(.*\)/\1/' )
+export BUILD_INFO_LOCATION=$(pwd)/artifactory-repo/build-info.json
 
-targetRepo="libs-release-local"
+java -jar /opt/concourse-release-scripts.jar promote $RELEASE_TYPE $BUILD_INFO_LOCATION || { exit 1; }
 
-echo "Promoting ${buildName}/${buildNumber} to ${targetRepo}"
-
-curl \
-	-s \
-	--connect-timeout 240 \
-	--max-time 900 \
-	-u ${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} \
-	-H "Content-type:application/json" \
-	-d "{\"status\": \"staged\", \"sourceRepo\": \"libs-staging-local\", \"targetRepo\": \"${targetRepo}\"}"  \
-	-f \
-	-X \
-	POST "${ARTIFACTORY_SERVER}/api/build/promote/${buildName}/${buildNumber}" > /dev/null || { echo "Failed to promote" >&2; exit 1; }
+java -jar /opt/concourse-release-scripts.jar \
+  --spring.config.location=${CONFIG_DIR}/release-scripts.yml \
+  distribute $RELEASE_TYPE $BUILD_INFO_LOCATION || { exit 1; }
 
 echo "Promotion complete"
 echo $version > version/version
