8f97ee08f7630ab694e132bc80cfa290cb9bb0eb
Stephane Nicoll <snicoll@vmware.com>
2021-06-02 10:17:12 +0200

    Make sure that Spring Boot versions are available
    Closes gh-1214

--- initializr-service-sample/src/main/java/sample/service/ServiceApplication.java
+++ initializr-service-sample/src/main/java/sample/service/ServiceApplication.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors.
+ * Copyright 2012-2021 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,9 +16,14 @@
 
 package sample.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.spring.initializr.web.support.SaganInitializrMetadataUpdateStrategy;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
@@ -33,6 +38,13 @@ public class ServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceApplication.class, args);
+	}
+
+	// This bean opt-in for fetching available Spring Boot versions from Sagan (spring.io)
+	@Bean
+	SaganInitializrMetadataUpdateStrategy saganInitializrMetadataUpdateStrategy(RestTemplateBuilder restTemplateBuilder,
+			ObjectMapper objectMapper) {
+		return new SaganInitializrMetadataUpdateStrategy(restTemplateBuilder.build(), objectMapper);
 	}
 
 }
