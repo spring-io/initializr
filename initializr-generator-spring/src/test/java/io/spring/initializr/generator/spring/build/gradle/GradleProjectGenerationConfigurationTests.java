d9fb4b05b467f37fbf51be8ecf620f934bcf575a
Brian Clozel <bclozel@vmware.com>
2022-12-20 10:17:14 +0100

    Upgrade to Gradle 7.6
    Closes gh-1361

--- initializr-generator-spring/src/test/java/io/spring/initializr/generator/spring/build/gradle/GradleProjectGenerationConfigurationTests.java
+++ initializr-generator-spring/src/test/java/io/spring/initializr/generator/spring/build/gradle/GradleProjectGenerationConfigurationTests.java
@@ -84,7 +84,7 @@ class GradleProjectGenerationConfigurationTests {
 	}
 
 	static Stream<Arguments> gradleWrapperParameters() {
-		return Stream.of(Arguments.arguments("2.2.3.RELEASE", "6.9.3"), Arguments.arguments("2.5.0", "7.5.1"));
+		return Stream.of(Arguments.arguments("2.2.3.RELEASE", "6.9.3"), Arguments.arguments("2.5.0", "7.6"));
 	}
 
 	@ParameterizedTest(name = "Spring Boot {0}")
