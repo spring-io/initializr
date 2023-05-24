8f73f6c286f31abbda943761e7573e22a97dc60e
Moritz Halbritter <mkammerer@vmware.com>
2023-05-24 10:34:10 +0200

    Polish

--- initializr-generator/src/main/java/io/spring/initializr/generator/project/ProjectDescription.java
+++ initializr-generator/src/main/java/io/spring/initializr/generator/project/ProjectDescription.java
@@ -42,7 +42,7 @@ public interface ProjectDescription {
 	}
 
 	/**
-	 * Return a immutable mapping of requested {@link Dependency dependencies}.
+	 * Return an immutable mapping of requested {@link Dependency dependencies}.
 	 * @return the requested dependencies
 	 */
 	Map<String, Dependency> getRequestedDependencies();
@@ -96,7 +96,7 @@ public interface ProjectDescription {
 	String getName();
 
 	/**
-	 * Return a human readable description of the project.
+	 * Return a human-readable description of the project.
 	 * @return the description of the project or {@code null}
 	 */
 	String getDescription();
