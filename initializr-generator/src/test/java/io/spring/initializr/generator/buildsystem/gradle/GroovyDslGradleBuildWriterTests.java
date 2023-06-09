c42ed2e89fb41d19f7bdf62526bb302619ef5bfb
Prithvi singh <prithvi.singh@1mg.com>
2023-06-09 20:46:22 +0530

    Revert "Updated test to check for java configuration. Also parameterized the test. gh-1420"
    This reverts commit 321d1f821763bc7e110d83c95b8de1b6487af9c8.

--- initializr-generator/src/test/java/io/spring/initializr/generator/buildsystem/gradle/GroovyDslGradleBuildWriterTests.java
+++ initializr-generator/src/test/java/io/spring/initializr/generator/buildsystem/gradle/GroovyDslGradleBuildWriterTests.java
@@ -18,7 +18,6 @@ package io.spring.initializr.generator.buildsystem.gradle;
 
 import java.io.StringWriter;
 import java.util.Comparator;
-import java.util.stream.Stream;
 
 import io.spring.initializr.generator.buildsystem.BillOfMaterials;
 import io.spring.initializr.generator.buildsystem.Dependency;
@@ -30,9 +29,6 @@ import io.spring.initializr.generator.io.SimpleIndentStrategy;
 import io.spring.initializr.generator.version.VersionProperty;
 import io.spring.initializr.generator.version.VersionReference;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -52,22 +48,11 @@ class GroovyDslGradleBuildWriterTests extends GradleBuildWriterTests {
 		assertThat(write(build).lines()).contains("group = 'com.example'", "version = '1.0.1-SNAPSHOT'");
 	}
 
-	@ParameterizedTest
-	@MethodSource("sourceCompatibilityParameters")
-	void gradleBuildWithSourceCompatibility(String sourceCompatibility, String javaVersionConstant) {
+	@Test
+	void gradleBuildWithSourceCompatibility() {
 		GradleBuild build = new GradleBuild();
-		build.settings().sourceCompatibility(sourceCompatibility);
-		String javaConfiguration = writeJavaConfiguration(javaVersionConstant);
-		assertThat(write(build)).contains(javaConfiguration);
-	}
-
-	static Stream<Arguments> sourceCompatibilityParameters() {
-		return Stream.of(Arguments.arguments("1.8", "JavaVersion.VERSION_1_8"),
-				Arguments.arguments("8", "JavaVersion.VERSION_1_8"),
-				Arguments.arguments(null, "JavaVersion.VERSION_11"),
-				Arguments.arguments("11", "JavaVersion.VERSION_11"),
-				Arguments.arguments("17", "JavaVersion.VERSION_17"),
-				Arguments.arguments("18", "JavaVersion.VERSION_HIGHER"));
+		build.settings().sourceCompatibility("11");
+		assertThat(write(build).lines()).contains("sourceCompatibility = '11'");
 	}
 
 	@Test
