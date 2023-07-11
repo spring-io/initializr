e91f3f3b16cb5f9ec1b145a3648fdcd98576852b
Stephane Nicoll <snicoll@vmware.com>
2023-07-11 11:40:18 +0200

    Add support for resolving plugin versions
    This commit adds support for resolving the versions of managed plugins
    in a POM.
    
    Closes gh-1438

--- initializr-version-resolver/src/main/java/io/spring/initializr/versionresolver/DefaultMavenVersionResolver.java
+++ initializr-version-resolver/src/main/java/io/spring/initializr/versionresolver/DefaultMavenVersionResolver.java
@@ -22,14 +22,24 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.maven.model.Model;
+import org.apache.maven.model.building.DefaultModelBuilder;
+import org.apache.maven.model.building.DefaultModelBuilderFactory;
+import org.apache.maven.model.building.DefaultModelBuildingRequest;
+import org.apache.maven.model.building.ModelBuildingException;
+import org.apache.maven.model.resolution.ModelResolver;
+import org.apache.maven.project.ProjectBuildingRequest;
+import org.apache.maven.project.ProjectModelResolver;
 import org.apache.maven.repository.internal.MavenRepositorySystemUtils;
 import org.eclipse.aether.DefaultRepositorySystemSession;
 import org.eclipse.aether.RepositorySystem;
 import org.eclipse.aether.RepositorySystemSession;
+import org.eclipse.aether.RequestTrace;
 import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.connector.basic.BasicRepositoryConnectorFactory;
 import org.eclipse.aether.graph.Dependency;
 import org.eclipse.aether.impl.DefaultServiceLocator;
+import org.eclipse.aether.impl.RemoteRepositoryManager;
 import org.eclipse.aether.internal.impl.DefaultRepositorySystem;
 import org.eclipse.aether.repository.LocalRepository;
 import org.eclipse.aether.repository.LocalRepositoryManager;
@@ -37,6 +47,9 @@ import org.eclipse.aether.repository.RemoteRepository;
 import org.eclipse.aether.resolution.ArtifactDescriptorException;
 import org.eclipse.aether.resolution.ArtifactDescriptorRequest;
 import org.eclipse.aether.resolution.ArtifactDescriptorResult;
+import org.eclipse.aether.resolution.ArtifactRequest;
+import org.eclipse.aether.resolution.ArtifactResolutionException;
+import org.eclipse.aether.resolution.ArtifactResult;
 import org.eclipse.aether.spi.connector.RepositoryConnectorFactory;
 import org.eclipse.aether.spi.connector.transport.TransporterFactory;
 import org.eclipse.aether.spi.locator.ServiceLocator;
@@ -44,14 +57,16 @@ import org.eclipse.aether.transport.http.HttpTransporterFactory;
 import org.eclipse.aether.util.repository.SimpleArtifactDescriptorPolicy;
 
 /**
- * A {@link DependencyManagementVersionResolver} that resolves versions using Maven
- * Resolver. Maven's default {@link LocalRepositoryManager} implementation is not
- * thread-safe. To avoid corruption of the local repository, interaction with the
- * {@link RepositorySystem} is single-threaded.
+ * A {@link MavenVersionResolver} that resolves versions using Maven Resolver. Maven's
+ * default {@link LocalRepositoryManager} implementation is not thread-safe. To avoid
+ * corruption of the local repository, interaction with the {@link RepositorySystem} is
+ * single-threaded.
  *
  * @author Andy Wilkinson
+ * @author Stephane Nicoll
  */
-class MavenResolverDependencyManagementVersionResolver implements DependencyManagementVersionResolver {
+@SuppressWarnings("removal")
+class DefaultMavenVersionResolver implements MavenVersionResolver, DependencyManagementVersionResolver {
 
 	private static final RemoteRepository mavenCentral = new RemoteRepository.Builder("central", "default",
 			"https://repo1.maven.org/maven2")
@@ -72,9 +87,11 @@ class MavenResolverDependencyManagementVersionResolver implements DependencyMana
 
 	private final RepositorySystemSession repositorySystemSession;
 
+	private final RemoteRepositoryManager remoteRepositoryManager;
+
 	private final RepositorySystem repositorySystem;
 
-	MavenResolverDependencyManagementVersionResolver(Path cacheLocation) {
+	DefaultMavenVersionResolver(Path cacheLocation) {
 		ServiceLocator serviceLocator = createServiceLocator();
 		DefaultRepositorySystemSession session = MavenRepositorySystemUtils.newSession();
 		session.setArtifactDescriptorPolicy(new SimpleArtifactDescriptorPolicy(false, false));
@@ -84,10 +101,16 @@ class MavenResolverDependencyManagementVersionResolver implements DependencyMana
 		session.setUserProperties(System.getProperties());
 		session.setReadOnly();
 		this.repositorySystemSession = session;
+		this.remoteRepositoryManager = serviceLocator.getService(RemoteRepositoryManager.class);
 	}
 
 	@Override
 	public Map<String, String> resolve(String groupId, String artifactId, String version) {
+		return resolveDependencies(groupId, artifactId, version);
+	}
+
+	@Override
+	public Map<String, String> resolveDependencies(String groupId, String artifactId, String version) {
 		ArtifactDescriptorResult bom = resolveBom(groupId, artifactId, version);
 		Map<String, String> managedVersions = new HashMap<>();
 		bom.getManagedDependencies()
@@ -96,6 +119,18 @@ class MavenResolverDependencyManagementVersionResolver implements DependencyMana
 			.forEach((artifact) -> managedVersions.putIfAbsent(artifact.getGroupId() + ":" + artifact.getArtifactId(),
 					artifact.getVersion()));
 		return managedVersions;
+	}
+
+	@Override
+	public Map<String, String> resolvePlugins(String groupId, String artifactId, String version) {
+		Model model = buildEffectiveModel(groupId, artifactId, version);
+		Map<String, String> managedPluginVersions = new HashMap<>();
+		model.getBuild()
+			.getPluginManagement()
+			.getPlugins()
+			.forEach((plugin) -> managedPluginVersions.putIfAbsent(plugin.getGroupId() + ":" + plugin.getArtifactId(),
+					plugin.getVersion()));
+		return managedPluginVersions;
 	}
 
 	private ArtifactDescriptorResult resolveBom(String groupId, String artifactId, String version) {
@@ -108,6 +143,40 @@ class MavenResolverDependencyManagementVersionResolver implements DependencyMana
 			catch (ArtifactDescriptorException ex) {
 				throw new IllegalStateException(
 						"Bom '" + groupId + ":" + artifactId + ":" + version + "' could not be resolved", ex);
+			}
+		}
+	}
+
+	private Model buildEffectiveModel(String groupId, String artifactId, String version) {
+		try {
+			ArtifactResult bom = resolvePom(groupId, artifactId, version);
+			RequestTrace requestTrace = new RequestTrace(null);
+
+			ModelResolver modelResolver = new ProjectModelResolver(this.repositorySystemSession, requestTrace,
+					this.repositorySystem, this.remoteRepositoryManager, repositories,
+					ProjectBuildingRequest.RepositoryMerging.POM_DOMINANT, null);
+			DefaultModelBuildingRequest modelBuildingRequest = new DefaultModelBuildingRequest();
+			modelBuildingRequest.setSystemProperties(System.getProperties());
+			modelBuildingRequest.setPomFile(bom.getArtifact().getFile());
+			modelBuildingRequest.setModelResolver(modelResolver);
+			DefaultModelBuilder modelBuilder = new DefaultModelBuilderFactory().newInstance();
+			return modelBuilder.build(modelBuildingRequest).getEffectiveModel();
+		}
+		catch (ModelBuildingException ex) {
+			throw new IllegalStateException(
+					"Model for '" + groupId + ":" + artifactId + ":" + version + "' could not be built", ex);
+		}
+	}
+
+	private ArtifactResult resolvePom(String groupId, String artifactId, String version) {
+		synchronized (this.monitor) {
+			try {
+				return this.repositorySystem.resolveArtifact(this.repositorySystemSession, new ArtifactRequest(
+						new DefaultArtifact(groupId, artifactId, "pom", version), repositories, null));
+			}
+			catch (ArtifactResolutionException ex) {
+				throw new IllegalStateException(
+						"Pom '" + groupId + ":" + artifactId + ":" + version + "' could not be resolved", ex);
 			}
 		}
 	}
--- initializr-version-resolver/src/main/java/io/spring/initializr/versionresolver/MavenVersionResolver.java
+++ initializr-version-resolver/src/main/java/io/spring/initializr/versionresolver/MavenVersionResolver.java
@@ -20,14 +20,13 @@ import java.nio.file.Path;
 import java.util.Map;
 
 /**
- * A {@code DependencyManagementVersionResolver} is used to resolve the versions in the
- * managed dependencies of a Maven bom. Implementations must be thread-safe.
+ * A {@code MavenVersionResolver} is used to resolve the versions of managed dependencies
+ * or plugins. Implementations must be thread-safe.
  *
  * @author Andy Wilkinson
- * @deprecated as of 0.20.0 in favor of {@link MavenVersionResolver}.
+ * @author Stephane Nicoll
  */
-@Deprecated(since = "0.20.0", forRemoval = true)
-public interface DependencyManagementVersionResolver {
+public interface MavenVersionResolver {
 
 	/**
 	 * Resolves the versions in the managed dependencies of the bom identified by the
@@ -38,17 +37,28 @@ public interface DependencyManagementVersionResolver {
 	 * @return the managed dependencies as a map of {@code groupId:artifactId} to
 	 * {@code version}
 	 */
-	Map<String, String> resolve(String groupId, String artifactId, String version);
+	Map<String, String> resolveDependencies(String groupId, String artifactId, String version);
 
 	/**
-	 * Creates a new {@code DependencyManagementVersionResolver} that uses the given
-	 * {@code location} for its local cache. To avoid multiple instances attempting to
-	 * write to the same location cache, callers should ensure that a unique location is
-	 * used. The returned resolver can then be used concurrently by multiple threads.
+	 * Resolves the versions in the managed plugins of the pom identified by the given
+	 * {@code groupId}, {@code artifactId}, and {@code version}.
+	 * @param groupId pom group ID
+	 * @param artifactId pom artifact ID
+	 * @param version pom version
+	 * @return the managed plugins as a map of {@code groupId:artifactId} to
+	 * {@code version}
+	 */
+	Map<String, String> resolvePlugins(String groupId, String artifactId, String version);
+
+	/**
+	 * Creates a new {@code MavenVersionResolver} that uses the given {@code location} for
+	 * its local cache. To avoid multiple instances attempting to write to the same
+	 * location cache, callers should ensure that a unique location is used. The returned
+	 * resolver can then be used concurrently by multiple threads.
 	 * @param location cache location
 	 * @return the resolver
 	 */
-	static DependencyManagementVersionResolver withCacheLocation(Path location) {
+	static MavenVersionResolver withCacheLocation(Path location) {
 		return new DefaultMavenVersionResolver(location);
 	}
 
