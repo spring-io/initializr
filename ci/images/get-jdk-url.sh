50cedf3dad562fb655981476ce4c18cd653f3bc9
Stephane Nicoll <snicoll@vmware.com>
2023-09-14 14:00:58 +0200

    Upgrade Java version in CI image

--- ci/images/get-jdk-url.sh
+++ ci/images/get-jdk-url.sh
@@ -3,7 +3,7 @@ set -e
 
 case "$1" in
 	java17)
-		 echo "https://github.com/bell-sw/Liberica/releases/download/17.0.8+7/bellsoft-jdk17.0.8+7-linux-amd64.tar.gz"
+		 echo "https://github.com/bell-sw/Liberica/releases/download/17.0.8.1+1/bellsoft-jdk17.0.8.1+1-linux-amd64.tar.gz"
 	;;
   *)
 		echo $"Unknown java version"
