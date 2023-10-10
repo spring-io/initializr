8bed115f6d15b5976ab89b6b669a8143051874a5
Stéphane Nicoll <snicoll@vmware.com>
2023-10-10 20:43:22 +0200

    Upgrade to Gradle 7.6.3
    Closes gh-1473

--- initializr-generator-spring/src/test/java/io/spring/initializr/generator/spring/build/gradle/GradleKtsProjectGenerationConfigurationTests.java
+++ initializr-generator-spring/src/test/java/io/spring/initializr/generator/spring/build/gradle/GradleKtsProjectGenerationConfigurationTests.java
@@ -86,7 +86,7 @@ class GradleKtsProjectGenerationConfigurationTests {
 	}
 
 	static Stream<Arguments> gradleWrapperParameters() {
-		return Stream.of(Arguments.arguments("2.2.3.RELEASE", "6.9.3"), Arguments.arguments("2.5.0", "7.6.2"),
+		return Stream.of(Arguments.arguments("2.2.3.RELEASE", "6.9.3"), Arguments.arguments("2.5.0", "7.6.3"),
 				Arguments.arguments("2.7.10", "8.2.1"));
 	}
 
