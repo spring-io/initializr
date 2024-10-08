f17b215e6c4f46a628cb1518ea3c7520c7c98e4b
sijun-yang <yangsijun5528@gmail.com>
2024-10-09 03:26:15 +0900

    Add hasGroupId method

--- initializr-generator-spring/src/main/java/io/spring/initializr/generator/spring/build/BuildMetadataResolver.java
+++ initializr-generator-spring/src/main/java/io/spring/initializr/generator/spring/build/BuildMetadataResolver.java
@@ -65,4 +65,14 @@ public class BuildMetadataResolver {
 		return dependencies(build).anyMatch((dependency) -> dependency.getFacets().contains(facet));
 	}
 
+	/**
+	 * Checks if the given {@link Build} contains dependencies with the given {@code groupId}.
+	 * @param build the build to query
+	 * @param groupId the groupId to query
+	 * @return {@code true} if this build defines at least a dependency with that groupId, {@code false} otherwise
+	 */
+	public boolean hasGroupId(Build build, String groupId) {
+		return dependencies(build).anyMatch((dependency) -> dependency.getGroupId().equals(groupId));
+	}
+
 }
