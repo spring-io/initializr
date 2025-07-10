c3688975f9567e9fbd71f079d5576b9bfd3129e0
sijun-yang <yangsijun5528@gmail.com>
2025-07-10 18:02:05 +0900

    Chore
    Signed-off-by: sijun-yang <yangsijun5528@gmail.com>

--- initializr-generator/src/test/java/io/spring/initializr/generator/language/MultipleAnnotationContainerTests.java
+++ initializr-generator/src/test/java/io/spring/initializr/generator/language/MultipleAnnotationContainerTests.java
@@ -16,10 +16,10 @@
 
 package io.spring.initializr.generator.language;
 
-import org.junit.jupiter.api.Test;
-
 import java.util.List;
 import java.util.stream.Collectors;
+
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
